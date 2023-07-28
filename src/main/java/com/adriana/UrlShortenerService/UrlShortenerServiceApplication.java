8b4601605eda569f11ba80e443adfb6f92588b99
Boris Chen <borischen0203@gmail.com>
2023-07-29 00:57:27 +0100

    Fix unit test and run error

--- src/main/java/com/adriana/UrlShortenerService/UrlShortenerServiceApplication.java
+++ src/main/java/com/adriana/UrlShortenerService/UrlShortenerServiceApplication.java
@@ -2,11 +2,9 @@ package com.adriana.UrlShortenerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableWebMvc
 @EnableSwagger2
 public class UrlShortenerServiceApplication {
 
